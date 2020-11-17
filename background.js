75741b9d1a56d41211a611673c54a709147f86fe
fullburnen <fullburnen@protonmail.com>
2020-11-17 01:22:38 -0600

    Add support for context menu access key

--- background.js
+++ background.js
@@ -1,6 +1,18 @@
+function get_menu_string() {
+    try {
+        browser.contextMenus.update( "background-tab_it-detect", { visible: true } );
+        return "&Background-tab Link";
+    }
+    catch( e ) {
+        return "Background-tab Link";
+    }
+}
+
+var menu_string = get_menu_string();
+
 browser.contextMenus.create({
     id: "background-tab_link",
-    title: "Background-tab Link",
+    title: menu_string,
     contexts: [ "link" ],
 });
 
